eb2b69c18359d5620b3d526f0c061644e1f0fc39
Ho Phuoc Vinh Hung <hophuocvinhhung@gmail.com>
2025-01-07 21:26:55 +0700

    UpdateSrc

--- app/src/main/java/com/example/onebitmoblie/LoginOneBit.java
+++ app/src/main/java/com/example/onebitmoblie/LoginOneBit.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
-public class LoginActivity extends Activity {
+public class LoginOneBit extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.loginonebit);
     }
 }
---
+++ app/src/main/java/com/example/onebitmoblie/profile/ProfileActivity.java
@@ -0,0 +1,4 @@
+package com.example.onebitmoblie.profile;
+
+public class ProfileActivity {
+}
