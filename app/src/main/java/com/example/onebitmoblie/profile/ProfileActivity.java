836948928da504f4d1ebdd114ae7e2630ab7a025
Ho Phuoc Vinh Hung <hophuocvinhhung@gmail.com>
2025-01-09 21:43:37 +0700

    detail profile UI

--- app/src/main/java/com/example/onebitmoblie/profile/ProfileActivity.java
+++ app/src/main/java/com/example/onebitmoblie/profile/ProfileActivity.java
@@ -1,4 +1,16 @@
 package com.example.onebitmoblie.profile;
 
-public class ProfileActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.onebitmoblie.R;
+
+public class ProfileActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profiledetail);
+    }
 }
