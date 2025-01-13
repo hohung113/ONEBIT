b333485471eb7be3a6e0617c763b4a5a48f758b7
Ho Phuoc Vinh Hung <hophuocvinhhung@gmail.com>
2025-01-13 20:57:49 +0700

    fix code

--- app/src/main/java/com/example/onebitmoblie/login_register/LoginActivity.java
+++ app/src/main/java/com/example/onebitmoblie/login_register/LoginActivity.java
@@ -1,4 +1,16 @@
 package com.example.onebitmoblie.login_register;
 
-public class LoginActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.onebitmoblie.R;
+
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
 }
