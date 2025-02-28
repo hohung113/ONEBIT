fc487183df1e8c0bd681a50bbf83665995af489a
nhatnguyen061 <nhatkarit000@gmail.com>
2025-02-28 21:09:07 +0700

    done btnFAQ+Disable btnHome

--- app/src/main/java/com/example/onebitmoblie/homepage/HomeActivity.java
+++ app/src/main/java/com/example/onebitmoblie/homepage/HomeActivity.java
@@ -5,15 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.onebitmoblie.R;
+import com.example.onebitmoblie.Schedule.ScheduleActivity;
 import com.example.onebitmoblie.profile.ProfileActivity;
 
 public class HomeActivity extends Activity {
     Button btnHome, btnTracking, btnNotification ,btnProfile ;
+    ImageButton btnFAQ;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,8 +27,11 @@ public class HomeActivity extends Activity {
         btnTracking = findViewById(R.id.btnTracking);
         btnNotification = findViewById(R.id.btnNotification);
         btnProfile = findViewById(R.id.btnProfile);
-        btnHome.setOnClickListener(view -> startActivity(new Intent(this, HomeActivity.class)));
+        btnFAQ = findViewById(R.id.btnFAQ);
+//        btnHome.setOnClickListener(view -> startActivity(new Intent(this, HomeActivity.class)));
         btnProfile.setOnClickListener(view -> startActivity(new Intent(this, ProfileActivity.class)));
+        btnFAQ.setOnClickListener(view -> startActivity(new Intent(this, ScheduleActivity.class)));
+
 
     }
 
