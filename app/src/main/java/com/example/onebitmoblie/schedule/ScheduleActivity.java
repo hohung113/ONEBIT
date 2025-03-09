2d3fa5951ddfc4e363240cb1f4cc8ac418ee9900
Quy Nguyen Duy <duyquy140903@gmail.com>
2025-03-09 21:17:35 +0700

    hot fix

--- app/src/main/java/com/example/onebitmoblie/schedule/ScheduleActivity.java
+++ app/src/main/java/com/example/onebitmoblie/schedule/ScheduleActivity.java
@@ -23,63 +23,48 @@ import com.example.onebitmoblie.R;
 import com.example.onebitmoblie.common.PopupHelper;
 import com.example.onebitmoblie.common.SessionManager;
 import com.example.onebitmoblie.homepage.HomeActivity;
+import com.example.onebitmoblie.login_register.RegisterActivity;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 import java.util.UUID;
 
 public class ScheduleActivity extends Activity {
-    private EditText title, description;
     private LinearLayout activityContainer;
-    private Button saveButton;
-    private ImageButton addActivityBtn;
     private TextView dateText;
     private Calendar calendar;
+
+    private EditText title;
+    private EditText description;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schedule);
 
+        activityContainer = findViewById(R.id.activity_container);
+
+        ImageButton addActivityBtn = findViewById(R.id.add_activity_btn);
+
+        addActivityBtn.setOnClickListener(v -> addNewActivity());
+
+        Button saveBtn = findViewById(R.id.save_btn);
+        saveBtn.setOnClickListener( v -> saveBtnClick());
+
         title = findViewById(R.id.txt_title);
         description = findViewById(R.id.txt_description);
-        activityContainer = findViewById(R.id.activity_container);
-        addActivityBtn = findViewById(R.id.add_activity_btn);
-        saveButton = findViewById(R.id.save_btn);
-        dateText = findViewById(R.id.schedule_date_txt);
+
+        //init today calendar
         calendar = Calendar.getInstance();
+        dateText = findViewById(R.id.schedule_date_txt);
 
-        addActivityBtn.setOnClickListener(v -> onAddActivityClick());
-        saveButton.setOnClickListener(v -> saveBtnClick());
         findViewById(R.id.schedule_date).setOnClickListener(v -> showDatePicker());
+
+        saveSelectedDate();
     }
 
-    public void onAddActivityClick() {
-        LayoutInflater inflater = LayoutInflater.from(this);
-        View cardView = inflater.inflate(R.layout.activity_card, activityContainer, false);
-
-        View deleteBtn = cardView.findViewById(R.id.remove_card);
-        deleteBtn.setOnClickListener(v -> activityContainer.removeView(cardView));
-
-        activityContainer.addView(cardView, activityContainer.getChildCount() - 1);
-    }
-    private void saveBtnClick() {
-        String scheduleID = UUID.randomUUID().toString();
-        String today = Calendar.getInstance().toString();
-        String userID = new SessionManager(this).getKeyId();
-
-        String fromDate = calendar.toString();
-        String toDate = calendar.toString();
-        String titleTxt = title.getText().toString();
-        String descriptionTxt = description.getText().toString();
-
-        //validation
-        if (titleTxt.isEmpty() || descriptionTxt.isEmpty()) {
-            PopupHelper.shopPopup(this, "Please don't let title or description empty!"
-                    , Color.RED, Color.WHITE);
-            return;
-        }
-    }
     private void showDatePicker()
     {
         Calendar today = Calendar.getInstance();
@@ -89,7 +74,7 @@ public class ScheduleActivity extends Activity {
                 this,
                 (view, year, month, dayOfMonth) -> {
                     calendar.set(year, month, dayOfMonth);
-                    //updateDateText();
+                    updateDateText();
                 },
                 today.get(Calendar.YEAR),
                 today.get(Calendar.MONTH),
@@ -101,8 +86,67 @@ public class ScheduleActivity extends Activity {
 
         datePickerDialog.show();
     }
-    public void goBack(android.view.View view){
-        onBackPressed();
+
+    private void updateDateText()
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        dateText.setText(sdf.format(calendar.getTime()));
     }
 
+    private void addNewActivity()
+    {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View cardView = inflater.inflate(R.layout.activity_card, activityContainer, false);
+
+        View deleteBtn = cardView.findViewById(R.id.remove_card);
+        deleteBtn.setOnClickListener(v -> activityContainer.removeView(cardView));
+
+        activityContainer.addView(cardView, activityContainer.getChildCount() - 1);
+    }
+
+    private void saveSelectedDate()
+    {
+        dateText.setText(calendar.getTime().toString());
+    }
+
+    private void saveBtnClick()
+    {
+        String scheduleID = UUID.randomUUID().toString();
+        String today = Calendar.getInstance().toString();
+        String userID = new SessionManager(this).getKeyId();
+
+        String fromDate = calendar.toString();
+        String toDate = calendar.toString();
+        String titleTxt = title.getText().toString();
+        String descriptionTxt = description.getText().toString();
+
+        //validation
+        if(titleTxt.isEmpty() || descriptionTxt.isEmpty())
+        {
+            PopupHelper.shopPopup(this, "Please don't let title or description empty!"
+                    , Color.RED, Color.WHITE);
+            return;
+        }
+
+        Scheduling s = new Scheduling(scheduleID, false, today, null, null,
+                titleTxt, userID, fromDate, toDate, descriptionTxt, SchedulingStatus.IN_PROGRESS);
+
+        saveScheduleFB(s);
+    }
+    private void saveScheduleFB(Scheduling schedule)
+    {
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("scheduling");
+        dbRef.child(schedule.getId()).setValue(schedule)
+                .addOnSuccessListener(aVoid -> {
+                    goHome();
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    PopupHelper.shopPopup(this," Save schedule fail\nError: " + e.getMessage(), Color.WHITE, Color.RED);
+                });
+    }
+    private void goHome()
+    {
+        startActivity(new Intent(this, HomeActivity.class));
+    }
 }
